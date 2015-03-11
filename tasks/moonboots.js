c233dcf963a9ea2180eaada4d951cf1e77c4955d
Matias <matiasdecarli@gmail.com>
2015-03-11 17:38:37 -0300

    closes #1

--- tasks/moonboots.js
+++ tasks/moonboots.js
@@ -11,13 +11,17 @@ var MoonbootsStatic = require('moonboots-static');
 
 
 module.exports = function(grunt) {
-  grunt.registerMultiTask('moonboots', 'A grunt task that use moontoobs-static to generate _build folder, using the same configuration file than the actual project', function() {  
-      var done = this.async();   
-      
-      var moonboots = new MoonbootsStatic(
-		this.data.config);      
-      console.log('outputting data to ',this.data.config.directory)
-      moonboots.on('log', console.log);
-      moonboots.on('ready', done);      
-  });
+    grunt.registerMultiTask('moonboots', 'A grunt task that use moontoobs-static to generate _build folder, using the same configuration file than the actual project', function() {
+        var done = this.async();
+
+        var moonboots = new MoonbootsStatic(
+            this.data.config);
+        console.log('outputting data to ', this.data.config.directory)
+        moonboots.on('log', function(type, detail) {
+            if (type && type[1] === 'error') {
+                grunt.fail.fatal(detail)
+            }
+        });
+        moonboots.on('ready', done);
+    });
 };
